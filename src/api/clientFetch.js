ad980b33ff399d195080f8795f01b1c670c020d2
Serhiei <Serhieievb@gmail.com>
2024-05-26 23:08:20 +0300

    set: onrender backend

--- src/api/clientFetch.js
+++ src/api/clientFetch.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const clientFetch = axios.create({
-  baseURL: 'http://localhost:3000/api/',
+  // baseURL: 'http://localhost:3000/api/',
+  baseURL: 'https://vue-proj-back.onrender.com/api/',
   withCredentials: true
-  // baseURL: 'https://vue-proj-back.onrender.com'
 })
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { authService } from '../api/authService'
 import GreetingView from '@/views/GreetingView.vue'
+import AuthView from '@/views/AuthView.vue'
+import HomePageView from '@/views/HomepageView.vue'
+import LoginView from '@/views/LoginView.vue'
+import RegistrationView from '@/views/RegistrationView.vue'
 
 // const GreetingView = () => import('@/views/GreetingView.vue')
-const AuthView = () => import('@/views/AuthView.vue')
-const HomePageView = () => import('@/views/HomepageView.vue')
-const LoginView = () => import('@/views/LoginView.vue')
-const RegistrationView = () => import('@/views/RegistrationView.vue')
+// const AuthView = () => import('@/views/AuthView.vue')
+// const HomePageView = () => import('@/views/HomepageView.vue')
+// const LoginView = () => import('@/views/LoginView.vue')
+// const RegistrationView = () => import('@/views/RegistrationView.vue')
 
 const BASE = import.meta.env.VITE_BASE_URL_FRONT
 
