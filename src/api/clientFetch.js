68d18d6be75c2aa66c23fd60400dcdcd0b2632d5
Serhiei <Serhieievb@gmail.com>
2024-05-27 08:34:35 +0300

    test: lazy loading

--- src/api/clientFetch.js
+++ src/api/clientFetch.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 
 export const clientFetch = axios.create({
   // baseURL: 'http://localhost:3000/api/',
+
   baseURL: 'https://vue-proj-back.onrender.com/api/',
   withCredentials: true
 })
