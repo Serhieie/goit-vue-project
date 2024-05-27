021d0ce6b16057474736e851bc41f0cf5a278bb0
Serhiei <Serhieievb@gmail.com>
2024-05-27 21:11:45 +0300

    fix: base

--- src/api/clientFetch.js
+++ src/api/clientFetch.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 export const clientFetch = axios.create({
-  baseURL: 'http://localhost:3000/api/',
+  // baseURL: 'http://localhost:3000/api/',
 
-  // baseURL: 'https://vue-proj-back.onrender.com/api/',
+  baseURL: 'https://vue-proj-back.onrender.com/api/',
   withCredentials: true
 })
